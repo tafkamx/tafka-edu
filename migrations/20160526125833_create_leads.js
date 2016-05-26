a9639a8c181faf48ec24e2604633a11fc12f10c9
Sergio de la Garza <sergio@delagarza.io>
2016-05-26 13:01:48 -0500

    Create leads migration

---
+++ migrations/20160526125833_create_leads.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('Leads', function(t) {
+    t.increments('id').primary();
+    t.string('email').unique();
+    t.string('name');
+    t.timestamps();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('Leads');
+};

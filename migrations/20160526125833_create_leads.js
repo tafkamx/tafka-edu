2a8fe6266e55a5945a21003a31d922e5f53a8e2a
Alvaro Cabrera <pateketrueke@gmail.com>
2016-05-26 19:01:54 -0500

    Add extra field for custom role; fix

--- migrations/20160526125833_create_leads.js
+++ migrations/20160526125833_create_leads.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('Leads', function(t) {
     t.increments('id').primary();
     t.string('email').unique();
+    t.string('role');
     t.string('name');
     t.timestamps();
   });

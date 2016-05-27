c06e4506a0c10774c334795cacaa25c2d5304fca
Alvaro Cabrera <pateketrueke@gmail.com>
2016-05-26 19:02:46 -0500

    Reuse the same view for all states

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -2,17 +2,17 @@ var HomeController = Class('HomeController').inherits(BaseController)({
 
   prototype : {
     index : function(req, res, next) {
-      res.render('home/index.html', {layout : 'application' });
+      res.render('home/index.html', {layout : 'application', status: {}, params: { email: '', name: '', role: '' } });
     },
 
     capture : function(req, res, next) {
       var lead = new Lead(req.body);
 
       lead.save().then(function(result) {
-        res.json(lead);
+        res.render('home/index.html', {layout : 'application', status: { ok: 1 }, params: req.body});
       })
       .catch(function(err) {
-        res.status(400).json(err);
+        res.status(400).render('home/index.html', {layout : 'application', status: { err: err }, params: req.body});
       });
 
     },
